cdb1459796af86da290e664d13d4f85545d76858
gsueur <gsueur@neogeo.fr>
2024-04-01 16:37:34 -0400

    handling of redirects in file download and better filename detection

--- server/services/fileService.py
+++ server/services/fileService.py
@@ -4,32 +4,39 @@ import os
 import re
 import mimetypes
 
+def guessFileNameFromURL(url):
+    filename = None
+    if '.' in url.split('/')[-1]:
+        filename = url.split('/')[-1]
+    return filename
+
 
 def downloadFile(url, destination_dir):
-    urllib.parse.unquote(url)
+    url = urllib.parse.unquote(url)
 
     r = requests.get(url, allow_redirects=True)
-    fileName = None
+    filename = guessFileNameFromURL(url)
     if r.status_code == 200:
         # let's try to guess the file format
         # when coming as an attachment, the filename is in the Content-Disposition header.
-
-        if 'Content-Disposition' in r.headers:
+        if r.history:
+            filename = guessFileNameFromURL(r.url)
+        if not filename and 'Content-Disposition' in r.headers:
             if (d := r.headers['Content-Disposition']) != 'inline':
                 names = re.findall("filename=(.+)", d)
                 if len(names) == 1:
-                    fileName = names[0]
+                    filename = names[0]
                 else:
-                    fileName = 'download'
-        if not fileName and 'Content-Type' in r.headers:
+                    filename = 'download'
+        if not filename and 'Content-Type' in r.headers:
             extension = mimetypes.guess_extension(r.headers['Content-Type'].split(';')[0])
-            fileName = 'download' + extension
+            filename = 'download' + extension
             # detect geojson format in a json file
             if extension == '.json' and "FeatureCollection" in str(r.content[:27]):
-                fileName = 'download.geojson'
-        if fileName:
-            fileName = os.path.join(destination_dir, fileName)
-            open(fileName, 'wb').write(r.content)
-        return fileName
+                filename = 'download.geojson'
+        if filename:
+            filename = os.path.join(destination_dir, filename)
+            open(filename, 'wb').write(r.content)
+        return filename
     else:
         return None
